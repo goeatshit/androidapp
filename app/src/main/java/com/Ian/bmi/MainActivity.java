5b6f70a815857b7b6f5673d90334e3de9c910e09
Ian <vastavisa@gmail.com>
2019-10-08 16:36:28 +0800

    BMI caclulation complete commit

--- app/src/main/java/com/Ian/bmi/MainActivity.java
+++ app/src/main/java/com/Ian/bmi/MainActivity.java
@@ -3,12 +3,27 @@ package com.Ian.bmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edWeight;
+    EditText edHeight ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bmi);
+        edWeight = findViewById(R.id.ed_weight);
+        edHeight = findViewById(R.id.ed_height);
+
     }
+    public void bmi (View view){
+        float weight = Float.parseFloat(edWeight.getText().toString());
+        float height = Float.parseFloat(edHeight.getText().toString());
+        float bmi = weight/(height*height);
+        System.out.println(bmi);
+        Log.d("mainActivity", "bmi: "+bmi);
+    }
+
 }
